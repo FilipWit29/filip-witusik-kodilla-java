6e0160a4499882c122a365a721f6d2a8e2ca4492
Filip Witusik <witusikf@gmail.com>
2023-10-12 20:46:51 +0200

    10.4

---
+++ kodilla-spring-demo/src/main/java/com/example/kodillaspringdemo/KodillaSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringDemoApplication.class, args);
+    }
+
+}
